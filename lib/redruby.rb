4cc171f6cce06018e4e24a17acda36b443f9baee
Brian Jordan <bcjordan@gmail.com>
2010-11-22 20:20:04 -0500

    Set up some classes

--- lib/redruby.rb
+++ lib/redruby.rb
@@ -1,3 +1,46 @@
-module Redruby
-  # Your code goes here...
+module RedRuby
+    class Submission
+        attr_accessor   :subreddit, :score, :url, :domain,
+                        :author, :is_self, :downs, :ups, :score
+                        :created_utc, :num_comments
+        
+        def initialize()
+        end
+        
+        # Alias upvotes and downvotes
+        def upvotes
+            self.ups
+        end
+        
+        def downvotes
+            self.downs
+        end
+        
+        # Returns printable datetime (UTC)
+        def date
+            Time.at(self.created_utc)
+        end
+    end
+    
+    class Comment
+        attr_accessor   :body, :body_html, :ups, :downs, :score, :replies, :author
+                        :num_replies
+                        
+    end
+    
+    class User
+        attr_accessor 
+    end
+        
+=begin
+    class RedRuby
+        def self.portray(food)
+            if food.downcase == "broccoli"
+                "Gross!"
+            else
+                "Delicious!"
+            end
+        end
+=end
+    end
 end
